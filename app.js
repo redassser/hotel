74d19090c3a675327a77685b082bd4d725b5b958
redassser <redryan890@gmail.com>
2020-08-22 19:26:21 -0400

    Making sure the bot actually works!!!

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Enmap = require('enmap');
+require('dotenv').config();
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require("fs");
